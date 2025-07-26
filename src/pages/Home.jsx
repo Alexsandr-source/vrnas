7ac5a99b5b4076871afcfc54d53c84a6c39dfe59
Александр Кузин <neonver.2020@gmail.com>
2025-07-26 21:09:54 +0900

    master

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,39 @@
 import NavBar from '../components/NavBar';
 import wave from '../assets/img/wave.png';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 import '../assets/scss/NavBar.scss'
+import '../assets/scss/Home.scss'
 
 function Home() {
   return (
     <>
         <img src={wave} className="wave-bg"/>
         <NavBar/>
-        <main className='Cool-cat'>
-          
+        <main>
+          <section>
+            <div>
+              <h1>
+                Immerse Yourself in Virtual Reality
+              </h1>
+              <p>
+                Experience Unforgettable Events in VR. Bring your events to life like never before with our VR services
+              </p>
+              <button>discover more</button>
+              <div>
+                <div>
+                  <div>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                  </div>
+                  <p><span>32k+</span> Happy Client</p>
+                </div>
+                <video width="50%" controls autoPlay loop muted/>
+              </div>
+            </div>
+            <img src="" alt="" />
+          </section>
         </main>
         <footer>
           
