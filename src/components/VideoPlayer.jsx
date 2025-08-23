5ca415b6460a62dd0c44022b04f5447a0cac2ef0
Александр Кузин <neonver.2020@gmail.com>
2025-08-23 22:22:58 +0900

    master

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -3,65 +3,60 @@ import playFirst from "../assets/img/play.svg";
 import "../assets/scss/VideoPlayer.scss";
 
 const VideoPlayer = ({ src, thumbnail }) => {
-	const com = document.querySelector('.com'),
-	videoHub = document.querySelector('.video__hud'),
-	video = document.querySelector('.video'),
-	videoPlayer = document.getElementById('video-player'),
-	progressLine = document.getElementById('video-hud__progress-line'),
-	actionButton = document.getElementById('video-hud__action'),
-	actionImage = document.querySelector('.video__hud__action_img')
+	const com = useRef(null),
+	videoHub = useRef(null),
+	video = useRef(null),
+	videoPlayer = useRef(null),
+	progressLine = useRef(null),
+	actionButton = useRef(null),
+	actionImage = useRef(null)
 
 
-	com.addEventListener('click', videoStart);
 	function videoStart() {
-		videoHub.style.display = 'block';
-		com.style.display = 'none';
-		videoPlayer.play();
+		videoHub.current.style.display = 'block';
+		com.current.style.display = 'none';
+		videoPlayer.current.play();
 	}
 	let observer = new IntersectionObserver(() => {
-		if (!videoPlayer.paused) {
-			videoPlayer.pause()
-		} else if(videoPlayer.currentTime !== 0) {
-			videoPlayer.play()
+		if (!videoPlayer.current.paused) {
+			videoPlayer.current.pause()
+		} else if(videoPlayer.current.currentTime !== 0) {
+			videoPlayer.current.play()
 		}
+		observer.observe(video)
 	}, { threshold: 0.4 })
-	observer.observe(video)
-	actionButton.addEventListener('click',videoAct);
-	videoPlayer.addEventListener('click',videoAct);
 	function videoAct() {
-		if(videoPlayer.paused) {
-			videoHub.style.display = 'block';
-			com.style.display = 'none';
-			videoPlayer.play();
-			actionImage.setAttribute('src', {src});
-			actionButton.setAttribute('className','video__hud__element video__hud__action video__hud__action_play');
+		if(videoPlayer.current.paused) {
+			videoHub.current.style.display = 'block';
+			com.current.style.display = 'none';
+			videoPlayer.current.play();
+			actionImage.current.setAttribute('src', {src});
+			actionButton.current.setAttribute('className','video__hud__element video__hud__action video__hud__action_play');
 		} else {
-			videoPlayer.pause();
-			actionImage.src = {src};
-			actionButton.setAttribute('className','video__hud__element video__hud__action video__hud__action_pause');
+			videoPlayer.current.pause();
+			actionImage.current.src = {src};
+			actionButton.current.setAttribute('className','video__hud__element video__hud__action video__hud__action_pause');
 		}
 	}
-	progressLine.addEventListener('click',videoChangeTime);
-	
 	function videoChangeTime(e) {
 		let mouseX = Math.floor(e.pageX - progressLine.offsetLeft);
 		let progress = mouseX / (progressLine.offsetWidth / 100);
-		videoPlayer.currentTime = videoPlayer.duration * (progress / 100);
+		videoPlayer.current.currentTime = videoPlayer.current.duration * (progress / 100);
 	}
 
 	
 	return (
-		<div className='video'>
-            <video className='video__player' id='video-player' src={src} thumbnail={thumbnail} preload='metadata'></video>
-            <div className="com" >
+		<div ref={video} className='video'>
+            <video ref={videoPlayer} className='video__player' onClick={videoAct} src={src} thumbnail={thumbnail} preload='metadata'></video>
+            <div ref={com} className="com" onClick={videoStart}>
                 <img src={playFirst} alt="play"/>
             </div>
-            <div className='video__hud'>
-                <div className='video__hud__element video__hud__action video__hud__action_play' id='video-hud__action'>
-                    <img className="video__hud__action_img" src="./assets/img/Arrow.svg" alt=""/>
+            <div ref={videoHub} className='video__hud'>
+                <div ref={actionButton} className='video__hud__element video__hud__action video__hud__action_play' onClick={videoAct}>
+                    <img ref={actionImage} className="video__hud__action_img" src="./assets/img/Arrow.svg" alt=""/>
                 </div>
                 <div className='video__hud__element video__hud__progress'>
-                    <div value='0' max='100' className='video__hud__element video__hud__progress_line' id='video-hud__progress-line'></div>
+                    <div ref={progressLine} value='0' max='100' className='video__hud__element video__hud__progress_line' onClick={videoChangeTime}></div>
                 </div>
             </div>
         </div>
