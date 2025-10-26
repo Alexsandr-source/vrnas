61622cc58279a0201d3ca8c90c83c5b85cdebb65
Александр Кузин <neonver.2020@gmail.com>
2025-10-26 18:53:03 +0900

    master

--- src/components/ECPcontainer.jsx
+++ src/components/ECPcontainer.jsx
@@ -5,23 +5,25 @@ import "../assets/scss/ECP.scss"
 
 function ECP() {
     return (
-        <div className="ECP-container">
-            <div className='ECP__col'>
-                <img className="ECP__col-img" src={ecpImg1} alt="" />
-                <h1 className="ECP__col-title">Expertise</h1>
-                <p className="ECP__col-text">Our team consists of experienced VR developers, designers, and technicians who have a passion for VR and a commitment to delivering quality work and give the best service</p>
+        <section className="ECP">
+            <div className="ECP-container">
+                <div className='ECP__col'>
+                    <img className="ECP__col-img" src={ecpImg1}/>
+                    <h1 className="ECP__col-title">Expertise</h1>
+                    <p className="ECP__col-text">Our team consists of experienced VR developers, designers, and technicians who have a passion for VR and a commitment to delivering quality work and give the best service</p>
+                </div>
+                <div className='ECP__col'>
+                    <img className="ECP__col-img" src={ecpImg2}/>
+                    <h1 className="ECP__col-title">Customization</h1>
+                    <p className="ECP__col-text">Every client is unique, and we believe every VR experience should be too. We'll work with you to create a customized solution that meets your specific needs and goals</p>
+                </div>
+                <div className='ECP__col'>
+                    <img className="ECP__col-img" src={ecpImg3}/>
+                    <h1 className="ECP__col-title">Service</h1>
+                    <p className="ECP__col-text">We believe in providing exceptional customer service, from initial consultation to final delivery. Our goal is to ensure you're satisfied with every aspect of your VR experience.</p>
+                </div>
             </div>
-            <div className='ECP__col'>
-                <img className="ECP__col-img" src={ecpImg2} alt="" />
-                <h1 className="ECP__col-title">Customization</h1>
-                <p className="ECP__col-text">Every client is unique, and we believe every VR experience should be too. We'll work with you to create a customized solution that meets your specific needs and goals</p>
-            </div>
-            <div className='ECP__col'>
-                <img className="ECP__col-img" src={ecpImg3} alt="" />
-                <h1 className="ECP__col-title">Service</h1>
-                <p className="ECP__col-text">We believe in providing exceptional customer service, from initial consultation to final delivery. Our goal is to ensure you're satisfied with every aspect of your VR experience.</p>
-            </div>
-        </div>
+        </section>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,7 @@ import VideoPlayer from '../components/VideoPlayer';
 import ECP from '../components/ECPcontainer';
 import VideoThumb from '../assets/img/thumbnail.png';
 import MyVideo from '../assets/video/video.mp4';
-import img from '../assets/img/bgLightMan.png'
 import '../assets/scss/Home.scss'
-
-import ecpImg1 from "../assets/img/ecpImg1.png";
-import ecpImg2 from "../assets/img/ecpImg1.png";
-import ecpImg3 from "../assets/img/ecpImg1.png";
 import "../assets/scss/ECP.scss"
 
 function Home() {
@@ -48,26 +43,7 @@ function Home() {
           </div>
           <img className="intro__image" src={normalMan} alt="normal man" />
         </section>
-
-
-        
-        <div className="ECP-container">
-            <div className='ECP__col'>
-                <img className="ECP__col-img" src={ecpImg1} alt="" />
-                <h1 className="ECP__col-title">Expertise</h1>
-                <p className="ECP__col-text">Our team consists of experienced VR developers, designers, and technicians who have a passion for VR and a commitment to delivering quality work and give the best service</p>
-            </div>
-            <div className='ECP__col'>
-                <img className="ECP__col-img" src={ecpImg2} alt="" />
-                <h1 className="ECP__col-title">Customization</h1>
-                <p className="ECP__col-text">Every client is unique, and we believe every VR experience should be too. We'll work with you to create a customized solution that meets your specific needs and goals</p>
-            </div>
-            <div className='ECP__col'>
-                <img className="ECP__col-img" src={ecpImg3} alt="" />
-                <h1 className="ECP__col-title">Service</h1>
-                <p className="ECP__col-text">We believe in providing exceptional customer service, from initial consultation to final delivery. Our goal is to ensure you're satisfied with every aspect of your VR experience.</p>
-            </div>
-        </div>
+        <ECP/>
       </main>
       <footer>
         
