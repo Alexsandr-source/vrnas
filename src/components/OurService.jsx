71d0c9608602f43efa563ae7fb2eecfa520667b3
Александр Кузин <neonver.2020@gmail.com>
2026-01-06 18:47:19 +0900

    master

--- src/components/OurService.jsx
+++ src/components/OurService.jsx
@@ -3,12 +3,70 @@ import "../assets/scss/OurService.scss"
 function OurService() {
     return (
         <div className="OurService-container">
-            <div className="OurService-col">
-                <p className="OurService-title">OUR SERVICE</p>
-                <h1 className="OurService-title-sub">Our Service</h1>
+            <div className="OurService-head">
+                <div className="OurService-col">
+                    <p className="OurService-title">OUR SERVICE</p>
+                    <h1 className="OurService-title-sub">Our Service</h1>
+                </div>
+                <div className="OurService-col">
+                    <p className="OurService-text">We use the latest VR hardware and software to create high-quality VR experiences that are accessible and affordable. Our goal is to provide exceptional customer service and support, and our team is always available to answer any questions and address any concerns you may have.</p>
+                </div>
             </div>
-            <div className="OurService-col">
-                <p className="OurService-text">We use the latest VR hardware and software to create high-quality VR experiences that are accessible and affordable. Our goal is to provide exceptional customer service and support, and our team is always available to answer any questions and address any concerns you may have.</p>
+            <div className="OurService-blocks">
+                <div className="OurService-block">
+                    <img src="" alt="" className="OurService-block-img" />
+                    <h2 className="OurService-block-title">
+                        VR Development
+                    </h2>
+                    <p className="OurService-block-text">
+                        From concept to creation, our team of VR developers will bring your vision to life.
+                    </p>
+                </div>
+                <div className="OurService-block">
+                    <img src="" alt="" className="OurService-block-img" />
+                    <h2 className="OurService-block-title">
+                        VR Development
+                    </h2>
+                    <p className="OurService-block-text">
+                        From concept to creation, our team of VR developers will bring your vision to life.
+                    </p>
+                </div>
+                <div className="OurService-block">
+                    <img src="" alt="" className="OurService-block-img" />
+                    <h2 className="OurService-block-title">
+                        VR Development
+                    </h2>
+                    <p className="OurService-block-text">
+                        From concept to creation, our team of VR developers will bring your vision to life.
+                    </p>
+                </div>
+                <div className="OurService-block">
+                    <img src="" alt="" className="OurService-block-img" />
+                    <h2 className="OurService-block-title">
+                        VR Development
+                    </h2>
+                    <p className="OurService-block-text">
+                        From concept to creation, our team of VR developers will bring your vision to life.
+                    </p>
+                </div>
+                <div className="OurService-block">
+                    <img src="" alt="" className="OurService-block-img" />
+                    <h2 className="OurService-block-title">
+                        VR Development
+                    </h2>
+                    <p className="OurService-block-text">
+                        From concept to creation, our team of VR developers will bring your vision to life.
+                    </p>
+                </div>
+                <div className="OurService-block">
+                    <img src="" alt="" className="OurService-block-img" />
+                    <h2 className="OurService-block-title">
+                        VR Development
+                    </h2>
+                    <p className="OurService-block-text">
+                        From concept to creation, our team of VR developers will bring your vision to life.
+                    </p>
+                </div>
             </div>
         </div>
     )
