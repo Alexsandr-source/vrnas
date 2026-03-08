f653ee8347a1f3526932b53c1535e9801bb1ace1
Александр Кузин <neonver.2020@gmail.com>
2026-03-09 00:00:33 +0900

    master

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -6,19 +6,37 @@ function Testimonial() {
     return (
         <div className="Testimonial">
             <div className="Testimonial-container">
-                <div className="inner-orbit">
-                    <img src={man} alt="man" />
-                    <div className="middle-orbit">
-                        <div className="middle-orbit-cirlces"></div>
 
-                        <div className="outer-orbit">
-                            <div className="outer-orbit-cirlces"></div>
-                        </div>
+                <h1>TESTIMONIAL</h1>
 
-                    </div>
-
-
+                <div className="center-circle">
+                    <img src={man} alt="" />
                 </div>
+
+                <div className="orbit orbit1">
+                    <div className="orbit1-cirlces"></div>
+                </div>
+
+                <div className="orbit orbit2">
+                    <div className="orbit2-cirlces"></div>
+                </div>
+
+                <div className="orbit orbit3">
+                    <div className="orbit3-cirlces"></div>
+                </div>
+
+                <div className="orbit orbit4">
+                    <div className="orbit4-cirlces"></div>
+                </div>
+
+                <div className="orbit orbit5">
+                    <div className="orbit5-cirlces"></div>
+                </div>
+
+                <div className="orbit orbit6">
+                    <div className="orbit6-cirlces"></div>
+                </div>
+
             </div>
         </div>
     )
